025fe547f0b977949ca7389a9ac7210a164209db
Fernando Albar <fernandoalbar@gmail.com>
2023-06-11 11:04:42 +0200

    first commit

---
+++ components/ui/Pagination.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Pagination from '@mui/material/Pagination';
+
+
+export  function Paginator() {
+  return (
+      <Pagination count={10} color="primary" />
+
+  );
+}
